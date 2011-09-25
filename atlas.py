f0b0d839accccc9a0e8b251dbb7591a537ec4c3a
Drew Crawford <drew@sealedabstract.com>
2011-09-25 16:44:34 -0500

    fix for 2392; the workflow appears basically stable

--- atlas.py
+++ atlas.py
@@ -2,9 +2,13 @@
 
 from config import get_config, project_with_name
 from work.gitConnect import GitConnect
-from work.work import projectIntegrate
+from work.work import projectIntegrate, PURGATORY_STMT
 from work.fogbugzConnect import FogBugzConnect
+from magic import magic
 WORK_DIR=".buildbot/"
+BAD_STATUS = [34]
+PURGATORY_TIME = 2
+
 import logging
 class Atlas:
     def __init__(self):
@@ -29,7 +33,7 @@ class Atlas:
             caseno = int(case["ixbug"])
             if not self.f.isReadyForTest(caseno): continue
             logging.info( "Invalidating "+case["ixbug"])
-            self.f.fbConnection.assign(ixBug=case["ixbug"],ixPersonAssignedTo=self.f.ixPerson,sEvent="Invalidation.  Are you getting unexpected e-mails after this case event?  File a bug against buildbot.")
+            self.f.fbConnection.assign(ixBug=case["ixbug"],ixPersonAssignedTo=self.f.ixPerson,sEvent="Invalidation.  Are you getting unexpected e-mails surrounding this case event?  File a bug against buildbot.")
         self.test_active_tickets()
     def test_active_tickets(self):
         projects = get_config()["Projects"]
@@ -53,14 +57,57 @@ class Atlas:
             if not test:
                 logging.warning("Ignoring case %d because it has no test case assigned." % caseno)
                 continue
+            if self.be_in_purgatory(parent,test,case,proj[0]):
+                logging.warning("Ignoring case %d because of purgatory." % caseno)
+                continue
             if self.test(case,proj[0]): #returns true if we integrate something
                 return self.test_active_tickets() #break out of this loop, because who knows what is happening with the list of active cases now, and re-test everything.
+    
+    
+    def be_in_purgatory(self,parent,test,casedetail,proj):
+        status_q = self.f.fbConnection.search(q=test,cols="ixStatus,dtResolved")
+
+        from dateutil.parser import parse
+        from dateutil.tz import tzlocal
+        import datetime
+        now = datetime.datetime.now().replace(tzinfo=tzlocal())
+        if int(status_q.ixstatus.contents[0]) in BAD_STATUS:
+            date = parse(status_q.dtresolved.contents[0])
+            if (now - date).seconds > PURGATORY_TIME * 60:
+                return False
+            #logging.info("case 1")
+            return True #in purgatory
+        event_q = self.events_since_glados(parent)
+        for event in event_q:
             
+            if not event.s.contents: continue
+            #logging.debug(event.s.contents[0].encode('utf-8'))
+            #logging.debug(PURGATORY_STMT)
+            if event.s.contents[0].encode('utf-8').startswith(PURGATORY_STMT):
+                date = parse(event.dt.contents[0])
+                if (now - date).seconds > PURGATORY_TIME * 60:
+                    self.test(casedetail,proj,forceFail=True) #escape from purgatory for this one... we need to force a failure to get it back to the implementer.
+                    return True #because of the above "escape hatch" we refuse to process this case as part of the outer loop
+                return True #leave the case in purgatory.
+        return False #not in purgatory to begin with
+                
+                
         
         
+    def events_since_glados(self,caseno):
+        events = self.f.fbConnection.search(q=caseno,cols="events")
+        out = []
+        for event in events.events:
+            if int(event.ixperson.contents[0])==magic.BUILDBOT_IXPERSON:
+                out = []
+                continue
+            out.append(event)
+        return out
+            
+        
             
             
-    def test(self,casedetail,proj):
+    def test(self,casedetail,proj,forceFail=False):
         logging.info("Testing %s" % casedetail)
         caseno = int(casedetail["ixbug"])
         test_error_statements = ["Would you like to know the results of that last test? Me too. If they existed, we'd all be VERY happy right now. And not furious, which is the emotion I'm actually feeling."]
@@ -81,10 +128,14 @@ class Atlas:
             self.glados_reassign(caseno,why=choice(test_error_statements)+" Merge failure:  can't merge %s into %d." % (integrate_to,caseno))
             return False
         git.mergeIn(integrate_to)
+        git.pushChangesToOriginBranch(branch="work-%d" % caseno)
         
         
         #todo: run actual tests
         passed = True
+        
+        if forceFail:
+            passed = False
         
         test_statements = ["The Enrichment Center regrets to inform you that this next test is impossible.","The Enrichment Center apologizes for this clearly broken test chamber.","Once again, the Enrichment Center offers its most sincere apologies on the occasion of this unsolvable test environment.","The Enrichment Center promises to always provide a safe testing environment.","In dangerous testing environments, the Enrichment Center promises to always provide useful advice.","When the testing is over, you will be missed.","Be advised that the next test requires exposure to uninsulated electrical parts that may be dangerous under certain conditions.","Enjoy this next test. I'm going to go to the surface. It's a beautiful day out. Yesterday I saw a deer. If you solve this next test, maybe I'll let you ride an elevator all the way up to the break room, and I'll tell you about the time I saw a deer again.","This next test involves turrets. You remember them, right? They're the pale spherical things that are full of bullets. Oh wait. That's you in five seconds. Good luck.","I have a surprise waiting for you after this next test. Telling you would spoil the surprise, so I'll just give you a hint: It involves meeting two people you haven't seen in a long time.","It says this next test was designed by one of Aperture's Nobel prize winners. It doesn't say what the prize was for. Well, I know it wasn't for Being Immune To Neurotoxin.","Federal regulations require me to warn you that this next test chamber... is looking pretty good.","I've got a surprise for you after this next test. Not a fake, tragic surprise like last time. A real surprise, with tragic consequences. And real confetti this time. The good stuff. Our last bag. Part of me's going to miss it, but at the end of the day it was just taking up space.","I'd love to help you solve the tests. But I can't.","These tests are potentially lethal when communication, teamwork, and mutual respect are not employed at all times. Naturally this will pose an interesting challenge for one of you, given the other's performance so far.","The upcoming tests require you to work together as a team.","For the sake of this test, I will pretend to be your partner.","Finally! I had almost given up hope of ever testing again.","For this next test, the humans originally requested helmets to avoid brain injuries. I ran the numbers. Making the goo deadly was more cost effective.","To get to the Vault, you are going to need to use all the tricks you have learned. To help, I have made these tests extremely difficult.","No one has ever completed this test before. The humans must have reconfigured it from my original plans.","I created this test to let the humans feel good about themselves. It is extremely easy. Just follow the arrows.","Before you leave, why don't we do one more test? For old time's sake...","I am not sure how I can make these tests any easier for you."]
         pass_statements = ["Excellent. Please proceed into the chamberlock after completing each test.","Very impressive. Please note that any appearance of danger is merely a device to enhance your testing experience.","Congratulations! The test is now over.","Okay. The test is over now. You win. Go back to the recovery annex. For your cake.","It was a fun test and we're all impressed at how much you won. The test is over. Come back.","Not bad. I forgot how good you are at this. You should pace yourself, though. We have A LOT of tests to do.","Well, you passed the test. I didn't see the deer today. I did see some humans. But with you here I've got more test subjects than I'll ever need.","Excellent! You're a predator and these tests are your prey. Speaking of which, I was researching sharks for an upcoming test. Do you know who else murders people who are only trying to help them?","Test chamber completed. In the interest of science, the Enrichment Center proudly presents the following list of numbers: 9 7 53 7 107.","'Test chamber completed. In the interest of science, the Enrichment...' [GLaDOS speaks over herself] I am now talking to you privately. Do not tell your teammate. Just between you and me, you're doing very well. '...107'","It would compromise the test to divulge individual scores. However, I can tell you at least one of you is doing very, very well.","You have a gift for these tests. That's not just flattery. You are great at science.","If you were human, you would want a reward for completing this test. A reward for testing...?","Well done. Interesting note, I only created this test to watch test subjects fail and you didn't. You must be very, very proud. I'm building the world's smallest trophy for you.","Excellent. I think you have earned a break from the official testing courses.","I think after that display, we should take a break from the official testing courses.","You just keep testing and testing. With perfect results and no consequences.","Congratulations on completing the Aperture Science standard cooperative testing courses.","Congratulations on completing the test. You two really are the best cooperative testing team I could ever ask for.","Congratulations on passing that test.","Congratulations, you managed to complete this absolutely meaningless test.","Congratulations. I am sure if I had the time to repair these tests, you would have never completed them. So again, congratulations on completing the broken easy tests.","Congratulations. Your ability to complete this test proves the humans wrong. They described it as impossible, deadly, cruel, and one test subject even had the nerve to call it broken."]
@@ -125,6 +176,7 @@ class Atlas:
          #let the implementer know how we did...
         self.f.fbConnection.assign(ixBug=caseno,ixPersonAssignedTo=self.f.findImplementer(caseno),sEvent=statement)
         
+
         
         if passed:
 
@@ -157,7 +209,9 @@ class Atlas:
             (parent,child) = self.f.getCaseTuple(caseno,oldTestCasesOK=True)
             ixPerson = self.f.optimalIxTester(ixPerson)
         else:
-            ixPerson = self.f.findCaseAreaixOwner(caseno)
+            ixPerson = self.f.findImplementer(caseno)
+            if magic.BUILDBOT_IXPERSON==ixPerson: #findImplementer returns the current assigned person as the default choice, which is Buildbot at this point
+                ixPerson = self.f.findCaseAreaixOwner(caseno)
         if reactivate:
             self.f.reactivate(caseno,ixPerson,msg=why)
         else:
